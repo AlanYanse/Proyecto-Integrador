6ecf64c0bcc0131c3fb92dc73c0078fa1cd2bfcd
Alan Yanse <yansealan@gmail.com>
2022-07-12 20:45:58 -0300

    agregando una cabecera

---
+++ myapp/src/app/componentes/tasks/tasks.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/service/task.service';
+import { Task } from 'src/app/Task';
+
+
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks: Task[] = [];
+
+  constructor(private taskService : TaskService) { }
+
+  ngOnInit(): void {
+    // Like promise
+    this.taskService.getTask().subscribe((task) => {
+      this.tasks = task;
+    } );
+  }
+
+  deleteTask(task : Task){
+    
+    this.taskService.deleteTask(task)
+    
+      .subscribe( 
+        
+        ()=>{
+
+            this.tasks = this.tasks.filter((t)=>{
+            console.log("task deleted");
+            return t.id !== task.id
+      })
+
+    })
+    
+  }
+
+  toggleReminder(task : Task){
+
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task)
+    .subscribe();
+  }
+
+  addTask(task : Task){
+
+    console.log(task);
+
+    this.taskService.addTask(task)
+    .subscribe(task =>{
+      this.tasks.push(task);
+    });
+
+  }
+
+
+
+}
