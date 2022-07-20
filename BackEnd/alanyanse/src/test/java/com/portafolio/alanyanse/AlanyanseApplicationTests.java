7316d27f9460c38050d324b5e6f85737e14c6853
Alan Yanse <yansealan@gmail.com>
2022-07-20 16:40:49 -0300

    arrancando con el backend

---
+++ BackEnd/alanyanse/src/test/java/com/portafolio/alanyanse/AlanyanseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.alanyanse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlanyanseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
